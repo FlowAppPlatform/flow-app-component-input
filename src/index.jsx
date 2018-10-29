172aa4d04a5ee2d3991dbb7d8ca66317f7ab8f48
Mohammed Faizuddin <faiz@avantari.org>
2018-10-29 20:12:56 +0530

    initial commit flow-app-component-input

---
+++ src/index.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import AppComponent from 'flow-app-component';
+
+import './css/theme/default.css';
+
+class InputComponent extends AppComponent {
+  constructor() {
+    super();
+    const newState = {
+      properties: [
+        {
+          categoryName: 'General',
+          categoryDescription: 'Basic settings for the input',
+          properties: [],
+        },
+        {
+          categoryName: 'Events',
+          categoryDescription: 'Events for the input',
+          properties: [
+            {
+              id: 'event',
+              name: 'Events',
+              type: 'graph',
+              options: {},
+              data: null,
+            },
+          ],
+        },
+      ],
+      iconUrl: '/assets/images/input-component.png',
+      name: 'Input',
+      type: 'ui-component',
+      componentType: 'input',
+      category: 'Views',
+      parent: null,
+      showOnComponentsPanel: true,
+      isValuable: true,
+      allowsChildren: false,
+    };
+
+    this.state = Object.assign(this.state, newState); // merge two states together, and dont lose any parent state properties.
+  }
+
+  renderContent() {
+    return (
+      <div style={{ width: '100%' }}>
+        <div className="input-container">
+          <label className="input-label" htmlFor="input-component">
+            Input Component
+          </label>
+          <div dir="ltr">
+            <div className="input-inner-container">
+              <div className="input-card">
+                <input
+                  type="text"
+                  autoComplete="text"
+                  className="input-component"
+                  id="input-component"
+                  name="input"
+                  placeholder="Input Placeholder"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default InputComponent;
